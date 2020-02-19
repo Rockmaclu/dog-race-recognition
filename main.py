98294e239a45047c9bbe78fc812da1bf0ba37e02
Manuel <manuchichi@hotmail.com>
2020-02-18 21:54:13 -0300

    Primer commit de la API en flask. Idea del funcionamiento general.

--- main.py
+++ main.py
@@ -1,156 +1,50 @@
-import os, sys
-import argparse
-import requests
-import psycopg2
-import torch
-import numpy as np 
 
-from config import config
-from img2vec_pytorch import Img2Vec
+import os
+import cv2
+from flask import Flask, request, redirect, url_for,jsonify
+from werkzeug.utils import secure_filename
+from flask import jsonify
+from tools import getCloseImages,loadImage
 from PIL import Image
 
-def convertArray(numpyArray):
-    newArray = []
-    for elem in numpyArray:
-        newArray.append(elem)
-    return newArray
+## AUXILIAR TOOLS ##
+def stringToRGB(base64_string):
+    imgdata = base64.b64decode(str(base64_string))
+    image = Image.open(io.BytesIO(imgdata))
+    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
 
-def tensorToString(embeddingsVector):
-    numpyVector = np.array(embeddingsVector.unsqueeze(0).tolist())
+## MAIN API ##
+
+app = Flask(__name__)
+
+@app.route('/api/query', methods=['POST'])
+def query():
+    if request.method == 'POST':
+
+        result = getCloseImages(stringToRGB((request.form['base64'])),request.form['range'])
         
-    stringVector = str(convertArray(numpyVector.flatten()))
+        resultData = {'closeImages': result} 
         
-    stringVector = '{' + stringVector[1:]
-    stringVector = stringVector[:-1] + '}'
-
-    return stringVector
-
-
-def loadPivots(path):
-
-    params = config() 
-    print('Connecting to the PostgreSQL database...')
-    conn = psycopg2.connect(**params)
-    cur = conn.cursor() 
-    
-    dirs = os.listdir(path)
-    i = 0
-    for item in dirs:
-        i = i + 1
-        image = Image.open(path+item)
-        embeddingsVector = extractVector(resizeImage(image))
-        
-        stringVector = tensorToString(embeddingsVector) 
-        cur.execute('INSERT INTO pivotes (id,vector,nivel) VALUES (%s,%s,%s);',[i,stringVector,i])
-
-    conn.commit()
-    cur.close()
-    if conn is not None:
-        conn.close()
-        print('Database connection closed.')
-
-def loadImages(path):
-
-    params = config() 
-    print('Connecting to the PostgreSQL database...')
-    conn = psycopg2.connect(**params)
-    cur = conn.cursor() 
-    
-    dirs = os.listdir(path)
-    for item in dirs:
-        image = Image.open(path+item)
-        embeddingsVector = extractVector(resizeImage(image))
-        
-        stringVector = tensorToString(embeddingsVector) 
-        cur.execute('INSERT INTO images (name,vector) VALUES (%s,%s);',[item,stringVector])
-
-    conn.commit()
-    cur.close()
-    if conn is not None:
-        conn.close()
-        print('Database connection closed.')
-
-def readImage(path):
-    return Image.open(path)
-
-def getNCloseImages(imagePath,n):
-        
-    params = config() 
-    print('Connecting to the PostgreSQL database...')
-    conn = psycopg2.connect(**params)
-    cur = conn.cursor() 
-    
-    image = Image.open(imagePath)
-    embeddingsVector = extractVector(resizeImage(image))
-    
-    stringVector = tensorToString(embeddingsVector)
-
-    cur.execute('SELECT queryimage(%s,%s);',[stringVector,n])
-
-    resultImages = cur.fetchall() 
-
-    print(resultImages)
-    
-    cur.close()
-    if conn is not None:
-        conn.close()
-        print('Database connection closed.')
-
-def cleanImage(imagePath):
-    response = requests.post(
-        'https://api.remove.bg/v1.0/removebg',
-        files={'image_file': open(imagePath, 'rb')},
-        data={'size': 'auto'},
-        headers={'X-Api-Key': 'xXGpv8ncn1vC7eXyZJ8RUP3K'},
-    )
-    if response.status_code == requests.codes.ok:
-        base=os.path.basename(imagePath)
-        with open(os.path.splitext(base)[0]+'.png', 'wb') as out:
-            out.write(response.content)
+        return jsonify(resultData)
     else:
-        print("Error:", response.status_code, response.text)
+        return jsonify({'message':'BAD REQUEST'})
 
-def resizeImage(im):
-    final_size = 224
-    size = im.size
-    ratio = float(final_size) / max(size)
-    new_image_size = tuple([int(x*ratio) for x in size])
-    im = im.resize(new_image_size, Image.ANTIALIAS)
 
-    new_im = Image.new("RGB", (final_size, final_size))
-    new_im.paste(im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
-
-    return new_im
-
-def extractVector(image):
-    img2vec = Img2Vec(cuda=False)
-    vec = img2vec.get_vec(image, tensor=True)
-    return vec
-
-def main():
-    parser = argparse.ArgumentParser(description='Tool for ')
-
-    parser.add_argument("--path", type=str, required=True, help="path of image or folder")
-    parser.add_argument("--action", choices=["queryImage", "loadImages","loadPivots"], required=True, type=str, help="action to apply")
-    parser.add_argument("--range", type=int, default=5, help="quantity of closest images (default = 5)")
-
-    args = parser.parse_args()
-
-    if args.action == 'queryImage':
-        #cleanImage(args.path)
-     
-        base = os.path.basename(args.path)
-        imageFullPath = os.path.abspath(os.path.splitext(base)[0] + '.png')
+@app.route('/api/loadImages', methods=['POST'])
+def loadImages():
+    if request.method == 'POST':
+        imageList = request.form.getlist('images')
             
-        listOfImages = getNCloseImages(imageFullPath,args.range)
+        for image_dic in imageList:
+            image = stringToRGB(image_dic['base64'])
+            name = image_dic['name']
 
-        print(listOfImages)
-
-    elif args.action == 'loadImages':
-        loadImages(args.path)
-
-    elif args.action == 'loadPivots':
-        loadPivots(args.path)
-
+            loadImage(image,name)
+        
+        return jsonify({'success':'true','message':'Imagenes cargadas con exito.'})
+    
+    else:
+        return jsonify({'sucess':'false','message':'BAD REQUEST'})
+        
 if __name__ == '__main__':
-    main()
+     app.run(port='5002')
